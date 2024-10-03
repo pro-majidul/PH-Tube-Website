c5e8b95ab4979d2d5f64e8c59b532af038f7a3e6
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-10-04 03:43:05 +0600

    card image absolute time and date added

--- js/work.js
+++ js/work.js
@@ -28,6 +28,28 @@ const ShowAllButtons = (buttons) => {
 
 
 
+/*
+Showallvodes function e jei card ta deya ace oi card er image er upore ekta second number deya ace oitake time a vag kore ber kora hoy 
+
+*/
+const timaDateCalculate = (times)=>{
+    const hour = parseInt(times/3600);
+    let remainingSecond = times % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`
+
+
+}
+
+/*
+  time date calculation ekhane ses hoy  
+ */
+
+
+
+
+
 // create video feath function 
 const loadVideoFeatch = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
@@ -44,11 +66,16 @@ const ShowAllVideos = (videos) => {
         const videoCard = document.createElement('div');
         videoCard.classList = "card card-compact bg-base-100 w-96 shadow-xl";
         videoCard.innerHTML = `
-        <figure class="h-[200px] ">
+        <figure class="h-[200px] relative">
             <img 
             src="${element.thumbnail}"
             class="h-full w-full object-cover"
             alt="Shoes" />
+            ${element.others.posted_date?.length == 0 ? "" : `
+                 <span class="absolute right-4 bottom-4 bg-black rounded p-1 text-xs text-white">
+                 ${ timaDateCalculate(element.others.posted_date)}</span>
+                 ` }
+           
         </figure>
         <div class="py-2 flex gap-2">
             <div class="h-10 w-10">
@@ -61,8 +88,10 @@ const ShowAllVideos = (videos) => {
             <h2 class=" font-bold text-lg">${element.title}</h2>
             <div class="flex items-center gap-3">
             <p class="text-gray-400">${element.authors[0].profile_name}</p>
-            <img class="h-5 w-5"
-             src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />
+            ${element.authors[0].verified == true ?
+                `<img class="h-5 w-5" 
+                src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`
+                : ""}
             </div>
             <p class="text-gray-600 text-xs">${element.others.views} views </p>
             </div>
